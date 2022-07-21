0f56a96c32ec44d2112251789dd1357cc7e70e0c
Eason <defend.sen@gmail.com>
2022-07-21 13:51:57 +1000

    fix: yup

--- src/components/CourtsForm/CourtForm.tsx
+++ src/components/CourtsForm/CourtForm.tsx
@@ -7,8 +7,6 @@ import {
   Heading,
   Input,
   useToast,
-  InputRightAddon,
-  InputGroup,
   Text,
 } from "@chakra-ui/react";
 import { Formik, Field, Form, ErrorMessage } from "formik";
@@ -26,22 +24,20 @@ type Props = {
 
 const CourtSchema = Yup.object().shape({
   name: Yup.string().min(5, "Too Short!").max(50, "Too Long!").required("Required"),
-  height: Yup.number().integer().positive().min(0).required("Required"),
-  length: Yup.number().integer().positive().min(0).required("Required"),
-  width: Yup.number().integer().positive().min(0).required("Required"),
-  centreCircleRadius: Yup.number().integer().positive().min(0).required("Required"),
-  lengthOfCorner: Yup.number().integer().positive().min(0).required("Required"),
-  lineBorderWidth: Yup.number().integer().positive().min(0).required("Required"),
-  restrictedAreaLength: Yup.number().integer().positive().min(0).required("Required"),
-  restrictedAreaWidth: Yup.number().integer().positive().min(0).required("Required"),
-  sideBorderWidth: Yup.number().integer().positive().min(0).required("Required"),
-  threePointLine: Yup.number().integer().positive().min(0).required("Required"),
-  threePointRadius: Yup.number().integer().positive().min(0).required("Required"),
+  length: Yup.number().integer().positive().min(1).required("Required"),
+  width: Yup.number().integer().positive().min(1).required("Required"),
+  centreCircleRadius: Yup.number().integer().positive().min(1).required("Required"),
+  lengthOfCorner: Yup.number().integer().positive().min(1).required("Required"),
+  lineBorderWidth: Yup.number().integer().positive().min(1).required("Required"),
+  restrictedAreaLength: Yup.number().integer().positive().min(1).required("Required"),
+  restrictedAreaWidth: Yup.number().integer().positive().min(1).required("Required"),
+  sideBorderWidth: Yup.number().integer().positive().min(1).required("Required"),
+  threePointLine: Yup.number().integer().positive().min(1).required("Required"),
+  threePointRadius: Yup.number().integer().positive().min(1).required("Required"),
   description: Yup.string(),
 });
 
 const CourtForm: React.FC<Props> = ({ header, courtData, API, method }) => {
-  console.log(courtData);
   const toast = useToast();
   return (
     <Formik
