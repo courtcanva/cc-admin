4267de6784e84c01895b930676a4ce9f6e580014
JIN <t19887348@hotmail.com>
2022-07-25 22:58:04 +1000

    feat: cc-108 typo fixed, logout func and remove no-use component

--- src/__tests__/components/Login/index.test.tsx
+++ src/__tests__/components/Login/index.test.tsx
@@ -5,7 +5,7 @@ describe("Login", () => {
   it("should render Login", () => {
     render(<Login />);
 
-    const loginText = screen.getByText(/Login in to CourtCanva Admin/i);
+    const loginText = screen.getByText(/Log in to CourtCanva Admin/i);
     expect(loginText).toBeInTheDocument();
   });
 });
