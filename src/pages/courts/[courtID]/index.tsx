333d1a6e19d612996a9524420fdf6ba53386d27b
JasonCaoAU <jason2019au@gmail.com>
2022-10-17 12:29:31 +0800

    feat: cc-0051 updated the code based on code review suggestion

--- src/pages/courts/[courtID]/index.tsx
+++ src/pages/courts/[courtID]/index.tsx
@@ -19,7 +19,8 @@ const CourtDetail = () => {
   useEffect(() => {
     const fetchCourts = async () => {
       const { data } = await api(apiUrl as string, { method: "get" });
-      ["_id", "isDeleted", "__v", "isHidden"].forEach((e) => delete data[e]);
+      console.log(data);
+      ["_id", "isDeleted", "isHidden"].forEach((e) => delete data[e]);
       data["createdAt"] = formatDate(data.createdAt);
       data["updatedAt"] = formatDate(data.updatedAt);
       setCourtData(data);
