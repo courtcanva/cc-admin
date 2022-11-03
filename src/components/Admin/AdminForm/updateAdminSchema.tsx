b3f62dff9930aed6d9efa17bd85fd1e8a7c6d438
shuzheng wang <shawn.wang86b@gmail.com>
2022-11-03 16:37:29 +1100

    fix: fix Yup files

--- src/components/Admin/AdminForm/updateAdminSchema.tsx
+++ src/components/Admin/AdminForm/updateAdminSchema.tsx
@@ -1,9 +1,9 @@
 import * as Yup from "yup";
 const updateAdminSchema = Yup.object().shape({
   name: Yup.string()
-  .min(3, "Name should contain at least 3 character")
-  .max(20, "Name should be no more than 20 characters")
-  .required("Required"),
+    .min(3, "Name should contain at least 3 character")
+    .max(20, "Name should be no more than 20 characters")
+    .required("Required"),
   permission: Yup.string().required("Required"),
 });
 
