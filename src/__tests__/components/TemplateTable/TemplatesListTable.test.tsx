7e5f73976900d1ab3975994f899154cc17f7c352
Zack Chen <zackchen5813@gmail.com>
2022-11-22 10:53:08 +1100

    fix: directly use of mocktemplatedata

--- src/__tests__/components/TemplateTable/TemplatesListTable.test.tsx
+++ src/__tests__/components/TemplateTable/TemplatesListTable.test.tsx
@@ -7,26 +7,25 @@ import userEvent from "@testing-library/user-event";
 import { TemplatesListHeads } from "@/constants/TemplatesHeads";
 
 describe("Template List table", () => {
-  const mockTemplateDate = mockTemplateData;
   it("Should render all table heads", () => {
-    renderWithMockedProvider(<TemplatesListTable templates={mockTemplateDate} />);
+    renderWithMockedProvider(<TemplatesListTable templates={mockTemplateData} />);
     const tableHeads = TemplatesListHeads;
     tableHeads.map((header) => expect(screen.getByText(header.title)).toBeInTheDocument());
   });
 
   it("Should correctly render the list data", () => {
-    renderWithMockedProvider(<TemplatesListTable templates={mockTemplateDate} />);
+    renderWithMockedProvider(<TemplatesListTable templates={mockTemplateData} />);
 
     expect(screen.getByRole("img")).toBeInTheDocument();
-    expect(screen.getByText(mockTemplateDate[0].design.designName)).toBeInTheDocument();
-    expect(screen.getByText(mockTemplateDate[0].design.designer)).toBeInTheDocument();
-    expect(screen.getByText(mockTemplateDate[0].user_id)).toBeInTheDocument();
-    expect(screen.getByText(mockTemplateDate[0].status)).toBeInTheDocument();
-    expect(screen.getByText(mockTemplateDate[0].tags.CourtType)).toBeInTheDocument();
+    expect(screen.getByText(mockTemplateData[0].design.designName)).toBeInTheDocument();
+    expect(screen.getByText(mockTemplateData[0].design.designer)).toBeInTheDocument();
+    expect(screen.getByText(mockTemplateData[0].user_id)).toBeInTheDocument();
+    expect(screen.getByText(mockTemplateData[0].status)).toBeInTheDocument();
+    expect(screen.getByText(mockTemplateData[0].tags.CourtType)).toBeInTheDocument();
   });
 
   it("The court preview modal is correctly load after click", () => {
-    renderWithMockedProvider(<TemplatesAuditTable templates={mockTemplateDate} />);
+    renderWithMockedProvider(<TemplatesAuditTable templates={mockTemplateData} />);
     const image = screen.getByRole("img");
     userEvent.click(image);
     expect(screen.getByTestId("court-preview")).toBeInTheDocument();
